a0d0ef8e0b99ad54fe49f44e38f52f773098464c
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2012-01-15 11:22:40 -0200

    Add modified generated JavaScript

--- js/app/index.js
+++ js/app/index.js
@@ -1,5 +1,5 @@
 (function() {
-  var decodePath, fetchSongListFromGithub, processSong, renderGithubSongsList, replaceSong, setupChordsVisibility, setupGithubIntegration, setupTransposition;
+  var decodePath, fetchSongListFromGithub, onInputChange, processSong, renderGithubSongsList, replaceSong, setupChordsVisibility, setupGithubIntegration, setupTransposition;
 
   $(function() {
     $('#customize').click(function() {
@@ -13,16 +13,21 @@
     $('#stylesheet textarea').on('change keyup', function() {
       return $('#songstyle').text($(this).val());
     }).change();
-    $('#input').on('change keyup mouseup input', function() {
-      $('#song').html(processSong($(this).val()));
-      $('#song tr.chords').toggle($('#show-real-chords')[0].checked);
-      $('#song tr.normalized-chords').toggle($('#show-normalized-chords')[0].checked);
-      return $('#song tr.lyrics').toggle($('#show-lyrics')[0].checked);
-    }).change();
+    $('#input').on('change keyup mouseup input', onInputChange).change();
     setupGithubIntegration();
     setupTransposition();
     return setupChordsVisibility();
   });
+
+  onInputChange = function() {
+    var tone;
+    $('#song').html(processSong($(this).val()));
+    tone = $('#transposition select').val();
+    if (tone) $('#song .tone').text(tone);
+    $('#song tr.chords').toggle($('#show-real-chords')[0].checked);
+    $('#song tr.normalized-chords').toggle($('#show-normalized-chords')[0].checked);
+    return $('#song tr.lyrics').toggle($('#show-lyrics')[0].checked);
+  };
 
   processSong = function(input) {
     var parser, tone;
@@ -75,6 +80,7 @@
     var base64Content;
     $('#remaining-github-requests').text(response.meta['X-RateLimit-Remaining']);
     base64Content = response.data.content;
+    $('#transposition select').val('');
     $('#input').val(decode(base64Content)).change();
     return $('#github-dialog').dialog('close');
   };
@@ -84,8 +90,7 @@
       return $('#transposition').dialog();
     });
     return $('#transposition select').on('change', function() {
-      $('#input').change();
-      if (this.value) return $('#song .tone').text(this.value);
+      return $('#input').change();
     });
   };
 
